14e2f2e4c83b61e81183539fa9ea66540dea026b
edihxl <edi.hoxhalli@cinarra.com>
2024-07-10 01:11:33 +0200

    ready steady go

--- app/exercise.tsx
+++ app/exercise.tsx
@@ -1,61 +1,79 @@
 import React, { useState, useEffect } from 'react';
 import { TouchableOpacity, View, StyleSheet, Animated } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
-import { useLocalSearchParams, useGlobalSearchParams, Link } from 'expo-router';
+import { useLocalSearchParams, Link } from 'expo-router';
 
 const ExerciseScreen: React.FC = () => {
-    const local = useLocalSearchParams();
-
     const { rounds } = useLocalSearchParams<{ rounds?: string }>();
+    const [titleText, setTitleText] = useState('Ready');
+    const [actionText, setActionText] = useState('Steady');
+    const [countdownText, setCountdownText] = useState('Go');
+    const initialCountdown = { 'Inhale': 4, 'Hold': 7, 'Exhale': 8, 'Total': 19 };
+    const [countdown, setCountdown] = useState({ ...initialCountdown });
+    const [currentRound, setCurrentRound] = useState(1);
+    const [currentPhase, setCurrentPhase] = useState<'Inhale' | 'Hold' | 'Exhale'>('Inhale');
+    const [timer, setTimer] = useState<number>(initialCountdown['Inhale']);
 
-    console.log('You selected: '+rounds + ' rounds');
     const [fadeAnimReady] = useState(new Animated.Value(0));
     const [fadeAnimSteady] = useState(new Animated.Value(0));
     const [fadeAnimGo] = useState(new Animated.Value(0));
 
+    // Animation function
+    const startAnimation = (
+        animatedValue: Animated.Value,
+        delay: number,
+        duration: number,
+        onComplete?: () => void
+    ): NodeJS.Timeout => {
+        return setTimeout(() => {
+            Animated.timing(
+                animatedValue,
+                {
+                    toValue: 1,
+                    duration,
+                    useNativeDriver: true
+                }
+            ).start(() => onComplete && onComplete());
+        }, delay);
+    };
+
     useEffect(() => {
-        // Animate "Ready"
-        const timeoutReady = setTimeout(() => {
-            Animated.timing(
-                fadeAnimReady,
-                {
-                    toValue: 1,
-                    duration: 500,  // Quicker animation
-                    useNativeDriver: true
-                }
-            ).start();
-        }, 300);  // Shorter delay
-
-        // Animate "Steady"
-        const timeoutSteady = setTimeout(() => {
-            Animated.timing(
-                fadeAnimSteady,
-                {
-                    toValue: 1,
-                    duration: 1000,
-                    useNativeDriver: true
-                }
-            ).start();
-        }, 1300);  // Longer delay
-
-        // Animate "Go"
-        const timeoutGo = setTimeout(() => {
-            Animated.timing(
-                fadeAnimGo,
-                {
-                    toValue: 1,
-                    duration: 1500,  // Longest duration
-                    useNativeDriver: true
-                }
-            ).start();
-        }, 2300);  // Longest delay
-
+        const timeoutReady = startAnimation(fadeAnimReady, 300, 500);
+        const timeoutSteady = startAnimation(fadeAnimSteady, 1300, 1000);
+        const timeoutGo = startAnimation(fadeAnimGo, 2300, 1500, afterAnimationsComplete);
+        
         return () => {
             clearTimeout(timeoutReady);
             clearTimeout(timeoutSteady);
             clearTimeout(timeoutGo);
         };
-    }, []);
+    }, [fadeAnimReady, fadeAnimSteady, fadeAnimGo]);
+
+    function afterAnimationsComplete() {
+        console.log('All animations completed!');
+        doTimer();
+        // additional logic as necessary
+    }
+
+    async function doTimer() {
+        
+        let roundsNum = rounds == undefined ? 1 : +rounds;
+        for (let currentRound = 1; currentRound <= roundsNum; currentRound++) {
+            setTitleText('Round '+currentRound+' of '+rounds);
+            for (let i = 0; i <= initialCountdown.Total; i++) {
+                await delay(1000);
+                console.log(i);   
+            }
+        }
+    }
+
+    function delay(delay: number) {
+        return new Promise(r => {
+            setTimeout(r, delay);
+        })
+    }
+
+    console.log('You selected: ' + rounds + ' rounds');
 
     return (
         <View style={styles.main}>
@@ -66,13 +84,13 @@ const ExerciseScreen: React.FC = () => {
             </Link>
             <View style={styles.instructions}>
                 <Animated.Text style={[styles.exerciseTitle, { opacity: fadeAnimReady }]}>
-                    Ready
+                    {titleText}
                 </Animated.Text>
                 <Animated.Text style={[styles.exerciseAction, { opacity: fadeAnimSteady }]}>
-                    Steady
+                    {actionText}
                 </Animated.Text>
                 <Animated.Text style={[styles.exerciseCountdown, { opacity: fadeAnimGo }]}>
-                    Go
+                    {countdownText}
                 </Animated.Text>
             </View>
         </View>
